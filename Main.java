4b577ef3dc2d34c39768b6d3ea7c9599bc58bdda
AdiL <46487031+Musaevv@users.noreply.github.com>
2019-05-14 23:52:48 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Random;
 
 public class Main implements ActionListener {
@@ -15,10 +14,10 @@ public class Main implements ActionListener {
     static ArrayList<Color> colors;
     static ArrayList<String> words;
     static Color chosenColor;
-    static int score = 0;
+    static int right = 0;
+    static int mistake = -1;
     static JLabel start;
-    static Random random = new Random(new Date().getTime());
-    static Timer timer;
+    static Random random = new Random();
 
     static void initColors() {
         colors = new ArrayList<>();
@@ -59,22 +58,18 @@ public class Main implements ActionListener {
         JLabel label = new JLabel(" Type in the colour of the words, and not the word text!");
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setPreferredSize(new Dimension(500,25));
-        label.setFont(new Font("Serfis",Font.BOLD,14));
+        label.setFont(new Font("Serfis",Font.LAYOUT_NO_LIMIT_CONTEXT,19));
         label.setForeground(Color.WHITE);
         panel.add(label);
+
         start = new JLabel(" Press enter to start");
         start.setHorizontalAlignment(SwingConstants.CENTER);
         start.setPreferredSize(new Dimension(400,25));
-        start.setFont(new Font("Start",Font.ITALIC,15));
+        start.setFont(new Font("Start",Font.ITALIC,16));
         start.setForeground(Color.WHITE);
         panel.add(start);
-        JLabel time = new JLabel(" Time left: ");
-        time.setHorizontalAlignment(SwingConstants.CENTER);
-        time.setPreferredSize(new Dimension(400,25));
-        time.setFont(new Font("Time",Font.ITALIC,15));
-        time.setForeground(Color.white);
-        panel.add(time);
-        color = new JLabel("Red") ;
+
+        color = new JLabel(" ") ;
         color.setPreferredSize(new Dimension(400, 200));
         color.setFont(new Font("ColorDisplayer", Font.BOLD, 50));
         color.setHorizontalAlignment(SwingConstants.CENTER);
@@ -86,8 +81,7 @@ public class Main implements ActionListener {
         textField.setPreferredSize(new Dimension(300, 20));
         textField.addKeyListener(new KeyListener() {
 
-            public void keyTyped(KeyEvent e) {
-            }
+            public void keyTyped(KeyEvent e) {}
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     checkAnswer();
@@ -98,34 +92,24 @@ public class Main implements ActionListener {
             public void keyReleased(KeyEvent e) {}
         });
         panel.add(textField);
-
-
-//setting the frame size
+        //setting the frame size
         frame.setSize(new Dimension(600, 400));
-//set the start position
+        //set the start position
         frame.setLocationRelativeTo(null);
-//set a default close action
+        //set a default close action
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//set a title
+        //set a title
         frame.setTitle("ColourGame");
-//Disable resize
-        frame.setResizable(false);
+        //Disable resize
         frame.setVisible(true);
     }
+
 
     public static void nextColor() {
         color.setText(words.get(random.nextInt(10)));
         chosenColor = colors.get(random.nextInt(9));
         color.setForeground(chosenColor);
-        try {
-            if (timer.isRunning()) {
-                timer.stop();
-            }
-        } catch (Exception ignored) {
-
-        }
     }
-
     private static void checkAnswer() {
         String usersColor = textField.getText().toLowerCase();
         System.out.println(usersColor + " " + chosenColor);
@@ -138,15 +122,16 @@ public class Main implements ActionListener {
                 chosenColor == Color.BLUE && usersColor.equals("blue") ||
                 chosenColor == Color.YELLOW && usersColor.equals("yellow") ||
                 chosenColor == Color.PINK && usersColor.equals("pink")){
-            score ++;
+            right ++;
+        }else {
+            mistake++;
         }
+        start.setText("Right: " + right + "" +
+                "      Misatakes: " + mistake);
 
-        start.setText("Score " + score);
         textField.setText("");
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
+    public void actionPerformed(ActionEvent e){}
 }
